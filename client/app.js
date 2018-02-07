295c17de3fa5a75208b83625eaa61180a42345db
孙昊翔 <673686754@qq.com>
2018-02-07 21:22:56 +0800

    热加载

--- client/app.js
+++ client/app.js
@@ -1,5 +1,23 @@
 import React from 'react'
 import { hydrate } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import App from './App.jsx'
 
-hydrate(<App/>, document.getElementById('root'))
+const root = document.getElementById('root')
+const render = Component => {
+  hydrate(
+    <AppContainer>
+      <Component/>
+    </AppContainer>,
+    root
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App.jsx', () => {
+    const NextApp = require('./App.jsx').default
+    render(NextApp)
+  })
+}
