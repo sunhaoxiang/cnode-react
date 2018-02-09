321a926984b85a9e9517edaacb61e39addfd8be0
孙昊翔 <673686754@qq.com>
2018-02-09 15:10:07 +0800

    no message

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
+import Layout from '../components/MyLayout.js'
+
 const About = () => (
-  <div>
+  <Layout>
     <p>This is the about page</p>
-  </div>
+  </Layout>
 )
 
 export default About
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Link from 'next/link'
+import Layout from '../components/MyLayout.js'
 
 const Index = () => (
-  <div>
-    <Link href="/about">
-      <a style={{ fontSize: 20 }}>About Page</a>
-    </Link>
+  <Layout>
     <p>Hello Next.js</p>
-  </div>
+  </Layout>
 )
 
 export default Index
