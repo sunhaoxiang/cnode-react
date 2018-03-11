8c0c64e6822ad180dc43ebb84d6cb725337e7c53
孙昊翔 <673686754@qq.com>
2018-03-11 12:05:54 +0800

    no message

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Routes from '../config/router';
+import { Link } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
-    return [
-      <div>123</div>,
-      <Routes />
-    ];
+    return (
+      <div>
+        <div>123</div>
+        <Routes />
+      </div>
+    );
   }
 };
