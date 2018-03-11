0aec6b3cee2c8067c0c7896fbb6b37b0d46c19e4
孙昊翔 <673686754@qq.com>
2018-03-11 13:40:42 +0800

    no message

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Router from '../config/router';
-import { Link } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
