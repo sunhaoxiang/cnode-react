84031fb0a7d23ca879d42aee43582b4d3af4dd33
孙昊翔 <673686754@qq.com>
2018-03-11 12:20:02 +0800

    no message

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Redirect
 } from 'react-router-dom';
 import TopicList from '../views/topic-list/index';
 import TopicDetail from '../views/topic-detail/index';
@@ -12,7 +13,8 @@ export default () => (
     <div>
       <Link to="/">首页</Link>
       <Link to="/detail">详情页</Link>
-      <Route path="/" component={TopicList} exact />
+      <Route path="/" render={() => <Redirect to="/list" />} exact />
+      <Route path="/list" component={TopicList}/>
       <Route path="/detail" component={TopicDetail} />
     </div>
   </Router>
